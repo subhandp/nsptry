b2ac9dcc92a9dfa5c7f97d32fb837f5bd636eeb2
subhan dinda putra <subhan.dinda.putra@gmail.com>
2017-08-13 11:27:03 +0800

    remove nbproject

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
-	$host = 'localhost';
-	$user = 'root';
-	$pass = '';
-	$db = 'nsp';
-	$con;
-        
-        $db = mysqli_connect($host,$user,$pass,$db);
-        $var = $db->query("SELECT * FROM nurse")or die($db->error);
-        print_r($var->fetch_all(MYSQLI_ASSOC));
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'nsp';
+$con;
+       
+$db = mysqli_connect($host,$user,$pass,$db);
+$var = $db->query("SELECT * FROM nurse")or die($db->error);
+$nurse = $var->fetch_all(MYSQLI_ASSOC);
+$nurseId = [];
+foreach ($nurse as $key => $value) {
+    array_push($nurseId, $value['nurse_id']);
+}
+        print_r($nurseId);
 
         
         
